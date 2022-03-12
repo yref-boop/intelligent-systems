e967f38b807d917a6d6574071c4fbb0a32d833c0
yref-boop <regofdez.yago@gmail.com>
2022-03-12 21:01:11 +0100

    1-a implemented

--- p1/src/es/udc/intelligentsystems/example/Node.java
+++ p1/src/es/udc/intelligentsystems/example/Node.java
@@ -8,24 +8,33 @@ public class Node {
   
     //attributes
     private Node parent;
-    public String action;
-    private State state;
+    public Action action;
+    public State state;
 
  
     //constructor
-    public Node(Node parent, String action, State state, Node next) {
+    public Node(Node parent, Action action, State state) {
         this.parent = parent;
         this.action = action;
         this.state = state;
     }
  
     //setter
-    public void setNextNode(Node node) {
-        this.next = node;
+    public void setParentNode(Node node) {
+        this.parent = node;
+    }
+    public void setAction(Action act){
+        this.action = act;
     }
  
     //getter
-    public Node getNextNode() {
-        return this.next;
-    } 
+    public Node getParentNode() {
+        return this.parent;
+    }
+    public Action getAction() {
+        return this.action;
+    }
+    public State getState(){
+        return this.state;
+    }
 }
--- p1/src/es/udc/intelligentsystems/example/Strategy4.java
+++ p1/src/es/udc/intelligentsystems/example/Strategy4.java
@@ -11,25 +11,31 @@ public class Strategy4 implements SearchStrategy {
 
     @Override
     public State solve(SearchProblem p) throws Exception{
-        ArrayList<State> explored = new ArrayList<State>();
+        ArrayList<Node> explored = new ArrayList<Node>();
         State currentState = p.getInitialState();
-        explored.add(currentState);
+        Node currentNode = new  Node(null, null, currentState);
+        explored.add(currentNode);
 
         int i = 1;
 
-        System.out.println((i++) + " - Starting search at " + currentState);
+        System.out.println((i++) + " - Starting search at " + currentNode.state);
 
-        while (!p.isGoal(currentState)){
-            System.out.println((i++) + " - " + currentState + " is not a goal");
-            Action[] availableActions = p.actions(currentState);
+        while (!p.isGoal(currentNode.state)){
+            System.out.println((i++) + " - " + currentNode.state + " is not a goal");
+            Action[] availableActions = p.actions(currentNode.state);
             boolean modified = false;
+            
             for (Action acc: availableActions) {
-                State sc = p.result(currentState, acc); 
-                System.out.println((i++) + " - RESULT(" + currentState + ","+ acc + ")=" + sc);
-                if (!explored.contains(sc)) {
-                    currentState = sc;
+                State sc = p.result(currentNode.state, acc); 
+                Node nextNode = new Node(currentNode, acc, sc);
+                System.out.println((i++) + " - RESULT(" + currentNode.state + ","+ acc + ")=" + sc);
+                
+                if (!(explored.stream().anyMatch(n -> n.state.equals(sc)))) {
+                    nextNode.setAction(acc);
+                    nextNode.setParentNode(currentNode);
+                    currentNode = nextNode;
                     System.out.println((i++) + " - " + sc + " NOT explored");
-                    explored.add(currentState);
+                    explored.add(currentNode);
                     modified = true;
                     System.out.println((i++) + " - Current state changed to " + currentState);
                     break;

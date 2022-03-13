57721e1c3c249ebae7572030ece62b660510631b
yref-boop <regofdez.yago@gmail.com>
2022-03-13 21:09:26 +0100

    added exercise 2 files

---
+++ p1/src/es/udc/intelligentsystems/MagicSquareProblem.java
@@ -0,0 +1,13 @@
+package es.udc.intelligentsystems.example;
+
+import es.udc.intelligentsystems.Action;
+import es.udc.intelligentsystems.State;
+import es.udc.intelligentsystems.SearchProblem;
+
+public class MagicSquareProblem extends SearchProblem {
+    public static class VacuumCleanerState extends State {
+        public
+
+    }
+
+}

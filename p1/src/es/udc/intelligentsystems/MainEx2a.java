f1dd0f58fcf71b3a79f0b693e7ca1e3ee6cb333f
yref-boop <regofdez.yago@gmail.com>
2022-03-15 10:26:47 +0100

    implemented main 2a, compiles, functionality to be checked

--- p1/src/es/udc/intelligentsystems/MainEx2a.java
+++ p1/src/es/udc/intelligentsystems/MainEx2a.java
@@ -9,9 +9,11 @@ public class MainEx2a {
 
     public static void main(String[] args) throws Exception {
 
-        //initial state -> {4,9,2,3,5,1,0,0,1,0} 
+        //initial state -> {4,9,2,3,5,0,0,1,0} 
 
-        ArrayList<Integer> list = new ArrayList<Integer> (4,9,2,3,5,1,0,0,1,0);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+            list.add(4); list.add(9); list.add(2); list.add(3); list.add(5); 
+            list.add(0); list.add(0); list.add(1); list.add(0);
 
         MagicSquareProblem.MagicSquareState initialState = new MagicSquareProblem.MagicSquareState(list);
         

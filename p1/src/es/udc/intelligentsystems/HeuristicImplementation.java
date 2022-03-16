fb512c7e8127590c0f5389254cfad98dacf7ea50
yref-boop <regofdez.yago@gmail.com>
2022-03-16 21:02:38 +0100

    solved problems on heuristic

--- p1/src/es/udc/intelligentsystems/HeuristicImplementation.java
+++ p1/src/es/udc/intelligentsystems/HeuristicImplementation.java
@@ -1,18 +1,23 @@
 package es.udc.intelligentsystems;
+import java.util.*;
+import es.udc.intelligentsystems.*;
 
-public abstract class Heuristic {
+public abstract class HeuristicImplemenation {
 
-    public float value(State s){
+    public float value(State st){
+        MagicSquareProblem.MagicSquareState s = (MagicSquareProblem.MagicSquareState) st;
+        int n = s.n;
         int wanted = (int) (s.n * ((s.n * s.n * s.n + s.n)/2));
         int actual = 0, proximity = 0;
 
         for (int l = 0; l < n; l++) {
-            for (int k = l*n; k < l*n + n; k++) { actual += state.values.get(k); }
+            for (int k = l*n; k < l*n + n; k++) { actual += s.values.get(k); }
         }
-        return floatValue(Math.abs(actual - wanted));
+        return (float)(Math.abs(actual - wanted));
     }
 
-    public float repetivity(State s) {
+    public float repetivity(State st) {
+        MagicSquareProblem.MagicSquareState s = (MagicSquareProblem.MagicSquareState) st;
         int duplicates = 0;
         ArrayList<Integer> array= s.values;
         List<Integer> checked = new ArrayList<>();
@@ -20,7 +25,7 @@ public abstract class Heuristic {
             if (checked.contains(integer)) duplicates++;
             else checked.add(integer);
         }
-        return floatValue(duplicates/(array.size()));
+        return (float)(duplicates/(array.size()));
     }
 
     public float evaluate(State e) {
